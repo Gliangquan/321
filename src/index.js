319e5f169869354c9a4543de5e07038ba29fa6a3
liangquan <liangquan0000@outlook.com>
2024-06-21 18:29:57 +0800

    111

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+// 将React组件挂载到DOM元素上
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
